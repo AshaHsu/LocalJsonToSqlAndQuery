82c036695658a6f8480598ce857c4e47c85be007
AshaHsu <hsuasha@gmail.com>
2016-04-12 02:20:47 -0400

    Get data from local Json file and save it into SQL, then query from database

---
+++ JsonToSqlAndQuery/Test.h
@@ -0,0 +1,22 @@
+//
+//  Test.h
+//  ProductProject
+//
+//  Created by AshaHsu on 2016-04-07.
+//  Copyright © 2016 AshaHsu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface Test : NSObject
+
+@property (nonatomic) NSString *index;
+@property (nonatomic) NSString *isActive;
+@property (nonatomic) NSString *balance;
+@property (nonatomic) NSString *income;
+@property (nonatomic) NSString *age;
+@property (nonatomic) NSString *company;
+@property (nonatomic) UIImage *picture;
+@property (nonatomic) NSDictionary *name;
+@end
---
+++ JsonToSqlAndQuery/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  ProductProject
+//
+//  Created by AshaHsu on 2016-04-07.
+//  Copyright © 2016 AshaHsu. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
